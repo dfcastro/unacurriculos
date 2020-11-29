9ca647c80dcea6a470dca385c74edab5ddb89ba9
dfcastro@outlook.com.br <dfcastro@outlook.com.br>
2020-11-28 21:55:51 -0300

    login admin

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Unacurriculos\Page;
-
+use \Unacurriculos\PageAdmin;
+use \Unacurriculos\Model\User;
 
 $app = new Slim();
 
@@ -17,7 +18,44 @@ $app->get('/', function() {
 });
 
 
+$app->get('/admin', function() {
+    User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("index");
+});
 
+$app->get ("/admin/login", function()
+{
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post ("/admin/login", function()
+{
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+	User::login($_POST["deslogin"], $_POST["despassword"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get("/admin/logout", function()
+{
+	User::logout();
+
+	header("Location: /admin/login");
+	exit();
+});
 
 
 $app->run();
